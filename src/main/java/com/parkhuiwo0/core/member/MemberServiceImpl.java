d365ea7e29c79c924b67762ab8a80b8560f9919d
parkhuiwo0 <huiwo0@naver.com>
2020-10-07 23:29:39 +0900

    order package 추가

--- src/main/java/com/parkhuiwo0/core/member/MemberServiceImpl.java
+++ src/main/java/com/parkhuiwo0/core/member/MemberServiceImpl.java
@@ -2,6 +2,9 @@ package com.parkhuiwo0.core.member;
 
 public class MemberServiceImpl implements MemberService {
 
+    /*
+    실제 할당하는 부분이 구현체에 의존하고 있다. (추상화와 구체화 둘 모두에게 의존하고 있음) -> DIP 위반
+     */
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     @Override
