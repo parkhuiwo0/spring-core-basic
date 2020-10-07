da2f0b6e970b78460515b42f53b21be361dc2fb8
herrenofficial <herren_official@naver.com>
2020-10-07 22:08:08 +0900

    init project

---
+++ src/test/java/com/parkhuiwo0/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parkhuiwo0.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
