649fbb45b00e6d1206bccd6ffcc75f8b5e34a35a
parkhuiwo0 <huiwo0@naver.com>
2020-10-12 23:22:10 +0900

    test : OrderServiceTest 추가

---
+++ src/test/java/com/parkhuiwo0/core/order/OrderServiceTest.java
@@ -0,0 +1,24 @@
+package com.parkhuiwo0.core.order;
+
+import com.parkhuiwo0.core.member.Grade;
+import com.parkhuiwo0.core.member.Member;
+import com.parkhuiwo0.core.member.MemberService;
+import com.parkhuiwo0.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+        Long memberId = 1L;
+        Member memberA = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(memberA);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
