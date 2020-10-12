10ddf02cbe7786c3bac63d5aa330d8808d44e599
parkhuiwo0 <huiwo0@naver.com>
2020-10-12 23:18:26 +0900

    feat : OrderApp 추가

---
+++ src/main/java/com/parkhuiwo0/core/OrderApp.java
@@ -0,0 +1,24 @@
+package com.parkhuiwo0.core;
+
+import com.parkhuiwo0.core.member.Grade;
+import com.parkhuiwo0.core.member.Member;
+import com.parkhuiwo0.core.member.MemberService;
+import com.parkhuiwo0.core.member.MemberServiceImpl;
+import com.parkhuiwo0.core.order.Order;
+import com.parkhuiwo0.core.order.OrderService;
+import com.parkhuiwo0.core.order.OrderServiceImpl;
+
+public class OrderApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        OrderService orderService = new OrderServiceImpl();
+
+        Long memberId = 1L;
+        Member memberA = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(memberA);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        System.out.println("order = " + order);
+        System.out.println("order = " + order.calculatePrice());
+    }
+}
