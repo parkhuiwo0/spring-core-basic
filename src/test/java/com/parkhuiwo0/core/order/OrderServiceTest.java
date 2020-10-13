31e14f1f1ce6f2c497afe873e94f12e8b3e55a16
parkhuiwo0 <huiwo0@naver.com>
2020-10-13 23:31:17 +0900

    MemberServiceTest, OrderServiceTest 의존성 외부주입

--- src/test/java/com/parkhuiwo0/core/order/OrderServiceTest.java
+++ src/test/java/com/parkhuiwo0/core/order/OrderServiceTest.java
@@ -1,16 +1,27 @@
 package com.parkhuiwo0.core.order;
 
+import com.parkhuiwo0.core.AppConfig;
 import com.parkhuiwo0.core.member.Grade;
 import com.parkhuiwo0.core.member.Member;
 import com.parkhuiwo0.core.member.MemberService;
 import com.parkhuiwo0.core.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class OrderServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
-    OrderService orderService = new OrderServiceImpl();
+//    MemberService memberService = new MemberServiceImpl();
+//    OrderService orderService = new OrderServiceImpl();
+    MemberService memberService;
+    OrderService orderService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        orderService = appConfig.orderService();
+    }
 
     @Test
     void createOrder() {
