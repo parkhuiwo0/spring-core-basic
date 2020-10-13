13ef39b249006c569b91b124f88d66a9f8f7961c
parkhuiwo0 <huiwo0@naver.com>
2020-10-13 23:35:47 +0900

    refactor : Appconfig 책임을 조금더 명확하게

--- src/main/java/com/parkhuiwo0/core/AppConfig.java
+++ src/main/java/com/parkhuiwo0/core/AppConfig.java
@@ -1,6 +1,8 @@
 package com.parkhuiwo0.core;
 
+import com.parkhuiwo0.core.discount.DiscountPolicy;
 import com.parkhuiwo0.core.discount.FixDiscountPolicy;
+import com.parkhuiwo0.core.member.MemberRepository;
 import com.parkhuiwo0.core.member.MemberService;
 import com.parkhuiwo0.core.member.MemberServiceImpl;
 import com.parkhuiwo0.core.member.MemoryMemberRepository;
@@ -10,11 +12,19 @@ import com.parkhuiwo0.core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 
 }
