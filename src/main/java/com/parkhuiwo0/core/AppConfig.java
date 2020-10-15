b8f9c6e59a6ce67559d1f3a70e48f28e06c07f53
parkhuiwo0 <huiwo0@naver.com>
2020-10-15 23:12:19 +0900

    AppConfig에 @Configuration 추가, @Bean 등록

--- src/main/java/com/parkhuiwo0/core/AppConfig.java
+++ src/main/java/com/parkhuiwo0/core/AppConfig.java
@@ -1,7 +1,6 @@
 package com.parkhuiwo0.core;
 
 import com.parkhuiwo0.core.discount.DiscountPolicy;
-import com.parkhuiwo0.core.discount.FixDiscountPolicy;
 import com.parkhuiwo0.core.discount.RateDiscountPolicy;
 import com.parkhuiwo0.core.member.MemberRepository;
 import com.parkhuiwo0.core.member.MemberService;
@@ -9,21 +8,28 @@ import com.parkhuiwo0.core.member.MemberServiceImpl;
 import com.parkhuiwo0.core.member.MemoryMemberRepository;
 import com.parkhuiwo0.core.order.OrderService;
 import com.parkhuiwo0.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
