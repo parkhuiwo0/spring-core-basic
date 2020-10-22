2dbb8ea007dadd46fed769a4b3691a71dcf8587a
parkhuiwo0 <huiwo0@naver.com>
2020-10-22 23:36:42 +0900

    refactor : StatefulService 싱글톤 문제점 해결

--- src/test/java/com/parkhuiwo0/core/singleton/StatefulService.java
+++ src/test/java/com/parkhuiwo0/core/singleton/StatefulService.java
@@ -1,15 +1,20 @@
 package com.parkhuiwo0.core.singleton;
 
+/**
+ * 공유되지 않는 지역변수, 파라미터, ThreadLocal 등을 사용해야 한다.
+ */
 public class StatefulService {
 
-    private int price; // 상태를 유지하는 필드
+//    private int price; // 상태를 유지하는 필드
 
-    public void order(String name, int price) {
+    public int order(String name, int price) {
         System.out.println("name = " + name + "price = " + price);
-        this.price = price; // 여기가 문제!
-    }
+//        this.price = price; // 여기가 문제!
 
-    public int getPrice() {
         return price;
     }
+
+//    public int getPrice() {
+//        return price;
+//    }
 }
--- src/test/java/com/parkhuiwo0/core/singleton/StatefulServiceTest.java
+++ src/test/java/com/parkhuiwo0/core/singleton/StatefulServiceTest.java
@@ -16,14 +16,15 @@ class StatefulServiceTest {
         StatefulService statefulService2 = ac.getBean(StatefulService.class);
 
         //ThreadA: A 사용자가 10000원을 주문
-        statefulService1.order("userA", 10000);
+        int userAPrice = statefulService1.order("userA", 10000);
 
         //ThreadB: B 사용자가 20000원을 주문
-        statefulService2.order("userB", 20000);
+        int userBPrice = statefulService2.order("userB", 20000);
 
         //ThreadA: A 사용자 주문 금액 조회
-        int price = statefulService1.getPrice();
-        assertThat(statefulService1.getPrice()).isEqualTo(20000);
+//        int price = statefulService1.getPrice();
+        System.out.println("userAPrice = " + userAPrice);
+//        assertThat(statefulService1.getPrice()).isEqualTo(20000);
 
     }
 
