2565bf1aaf2cce50f7c9c2f1cfb0ceb57c5ebf67
parkhuiwo0 <huiwo0@naver.com>
2020-10-26 23:16:52 +0900

    test : 컴포넌트 스캔과 의존관계 주입

---
+++ src/test/java/com/parkhuiwo0/core/scan/AutoAppConfigTest.java
@@ -0,0 +1,21 @@
+package com.parkhuiwo0.core.scan;
+
+import com.parkhuiwo0.core.AutoAppConfig;
+import com.parkhuiwo0.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class AutoAppConfigTest {
+
+    @Test
+    void basicScan() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
+
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+
+    }
+}
