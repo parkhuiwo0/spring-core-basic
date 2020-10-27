3fab190eef91717bfd44d2ba379774bf4ccc43d6
parkhuiwo0 <huiwo0@naver.com>
2020-10-27 23:36:30 +0900

    exclude filter / include filter 추가

---
+++ src/test/java/com/parkhuiwo0/core/scan/filter/BeanA.java
@@ -0,0 +1,5 @@
+package com.parkhuiwo0.core.scan.filter;
+
+@MyIncludeComponent
+public class BeanA {
+}
---
+++ src/test/java/com/parkhuiwo0/core/scan/filter/BeanB.java
@@ -0,0 +1,5 @@
+package com.parkhuiwo0.core.scan.filter;
+
+@MyExcludeComponent
+public class BeanB {
+}
