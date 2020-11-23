c795f9eff2e187bed5b020c4e11e8584e8e2915e
parkhuiwo0 <huiwo0@naver.com>
2020-11-23 22:40:12 +0900

    @Bean 설정 정보를 활용한 초기화 소멸 메서드 지정

--- src/test/java/com/parkhuiwo0/core/lifecycle/NetworkClient.java
+++ src/test/java/com/parkhuiwo0/core/lifecycle/NetworkClient.java
@@ -3,7 +3,9 @@ package com.parkhuiwo0.core.lifecycle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient
+//        implements InitializingBean, DisposableBean
+{
 
     private String url;
 
@@ -31,16 +33,27 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close : " + url);
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        System.out.println("NetworkClient.afterPropertiesSet");
+//    @Override
+//    public void afterPropertiesSet() throws Exception {
+//        System.out.println("NetworkClient.afterPropertiesSet");
+//        connect();
+//        call("초기화 연결 메시지");
+//    }
+//
+//    @Override
+//    public void destroy() throws Exception {
+//        System.out.println("NetworkClient.destroy");
+//        disConnect();
+//    }
+
+    public void init() throws Exception {
+        System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
-    @Override
-    public void destroy() throws Exception {
-        System.out.println("NetworkClient.destroy");
+    public void close() throws Exception {
+        System.out.println("NetworkClient.close");
         disConnect();
     }
 }
