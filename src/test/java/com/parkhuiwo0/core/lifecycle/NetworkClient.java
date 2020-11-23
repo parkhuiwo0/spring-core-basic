78f2e401e10d34ef4b1f09cee6bffe95c00d5c2d
parkhuiwo0 <huiwo0@naver.com>
2020-11-23 22:41:07 +0900

    @PostConstruct, @PreDestory를 이용한 초기화 소멸 메서드 지정

--- src/test/java/com/parkhuiwo0/core/lifecycle/NetworkClient.java
+++ src/test/java/com/parkhuiwo0/core/lifecycle/NetworkClient.java
@@ -3,6 +3,9 @@ package com.parkhuiwo0.core.lifecycle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class NetworkClient
 //        implements InitializingBean, DisposableBean
 {
@@ -46,12 +49,14 @@ public class NetworkClient
 //        disConnect();
 //    }
 
+    @PostConstruct
     public void init() throws Exception {
         System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
+    @PreDestroy
     public void close() throws Exception {
         System.out.println("NetworkClient.close");
         disConnect();
